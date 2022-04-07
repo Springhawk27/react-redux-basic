26665994bb4405048be9d5988e485a6c51ffd0f6
Springhawk27 <sajjad0mahmud@gmail.com>
2022-04-07 16:40:41 +0600

    basic redux functionalities created

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useDispatch } from "react-redux";
+import { login } from "../../features/user"
+
 
 function Login() {
+
+    const dispatch = useDispatch();
+
     return (
         <div>
-            <button>Login</button>
-        </div>
+            <button onClick={() => {
+                dispatch(login({ name: "Sajjad Mahmud", age: 27, email: "sajjad0mahmud@gmail.com" }))
+            }}>Login</button>
+        </div >
     )
 }
 
